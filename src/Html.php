bfe1d15c7d72586c145418c8e52dbb7ba3d747a4
ZoDream <648383079@qq.com>
2017-12-17 21:43:55 +0800

    修改

--- src/Html.php
+++ src/Html.php
@@ -105,4 +105,34 @@ class Html {
         $url .= implode('/', $rst);
         return str_replace('\\', '/', $url);
     }
+
+    public static function toText($str) {
+        $str = preg_replace("/<style .*?<\\/style>/is", "", $str);
+        $str = preg_replace("/<script .*?<\\/script>/is", "", $str);
+        $str = preg_replace("/<br \\s*\\/>/i", ">>>>", $str);
+        $str = preg_replace("/<\\/?p>/i", ">>>>", $str);
+        $str = preg_replace("/<\\/?td>/i", "", $str);
+        $str = preg_replace("/<\\/?div>/i", ">>>>", $str);
+        $str = preg_replace("/<\\/?blockquote>/i", "", $str);
+        $str = preg_replace("/<\\/?li>/i", ">>>>", $str);
+        $str = preg_replace("/ /i", " ", $str);
+        $str = preg_replace("/ /i", " ", $str);
+        $str = preg_replace("/&/i", "&", $str);
+        $str = preg_replace("/&/i", "&", $str);
+        $str = preg_replace("/</i", "<", $str);
+        $str = preg_replace("/</i", "<", $str);
+        $str = preg_replace("/“/i", '"', $str);
+        $str = preg_replace("/&ldquo/i", '"', $str);
+        $str = preg_replace("/‘/i", "'", $str);
+        $str = preg_replace("/&lsquo/i", "'", $str);
+        $str = preg_replace("/'/i", "'", $str);
+        $str = preg_replace("/&rsquo/i", "'", $str);
+        $str = preg_replace("/>/i", ">", $str);
+        $str = preg_replace("/>/i", ">", $str);
+        $str = preg_replace("/”/i", '"', $str);
+        $str = preg_replace("/&rdquo/i", '"', $str);
+        $str = strip_tags($str);
+        $str = html_entity_decode($str, ENT_QUOTES, "utf-8");
+        return preg_replace("/&#.*?;/i", "", $str);
+    }
 }
