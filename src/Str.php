d62214b69c2715eed7b05f0f64f42ece4c945347
zodream <648383079@qq.com>
2018-06-28 19:21:54 +0800

    增加驼峰转下划线

--- src/Str.php
+++ src/Str.php
@@ -321,6 +321,16 @@ class Str {
         return str_replace(' ', '', $value);
     }
 
+    /**
+     * 驼峰转下划线
+     * @param string $camelCaps
+     * @param string $separator
+     * @return string
+     */
+    public function unStudly($camelCaps, $separator='_') {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
+    }
+
 	/**
 	 * UTF8字符串的长度
 	 * @param string $str
