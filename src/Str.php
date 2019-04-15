98c533a0a50547bd8e3c97ab750d2d15f8e58b49
ZoDream <648383079@qq.com>
2019-04-15 23:18:37 +0800

    修改

--- src/Str.php
+++ src/Str.php
@@ -328,6 +328,10 @@ class Str {
 		return preg_replace('/'.$search.'$/', $replace, $arg, 1);
 	}
 
+    public static function parseCallback($callback, $default = null) {
+        return static::contains($callback, '@') ? explode('@', $callback, 2) : [$callback, $default];
+    }
+
     /**
      * Convert a value to studly caps case.
      *
