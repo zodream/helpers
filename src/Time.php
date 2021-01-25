1ec73c9a511bee028a671391eb0801f88e2e2e89
ZoDream <648383079@qq.com>
2021-01-25 12:39:55 +0800

    修复类型问题

--- src/Time.php
+++ src/Time.php
@@ -182,9 +182,9 @@ class Time {
      */
     public static function hoursFormat(int $time): string {
         return sprintf('%s:%s:%s',
-            str_pad(floor($time / 3600), 2, '0', STR_PAD_LEFT),
-            str_pad(floor($time % 3600 / 60), 2, '0', STR_PAD_LEFT),
-            str_pad(floor($time % 60), 2, '0', STR_PAD_LEFT)
+            str_pad((string)floor($time / 3600), 2, '0', STR_PAD_LEFT),
+            str_pad((string)floor($time % 3600 / 60), 2, '0', STR_PAD_LEFT),
+            str_pad((string)floor($time % 60), 2, '0', STR_PAD_LEFT)
         );
     }
 
